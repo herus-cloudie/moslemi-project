2b2770a4979991056ca5919df138207850b4a224
Amir <herus2006cripto.gmail.com>
2024-04-03 16:27:49 +0330

    last commit

--- src/Components/Dashboard/Content/WorkSamples/New/NewWorkSample.jsx
+++ src/Components/Dashboard/Content/WorkSamples/New/NewWorkSample.jsx
@@ -76,36 +76,36 @@ function NewWorkSample() {
    !addCategory && !deleteCategory?
     <div className='flex flex-col w-full 2xl:w-[70%] opacity-motion'>
         <div className='w-full bg-[#C0D9DB] p-2'>
-            <h1 className='font-semibold text-lg text-stone-800'>افزودن</h1>
+            <h1 className='font-[vasirMedium] text-lg text-stone-800'>افزودن</h1>
         </div>
         <form className='flex flex-col items-center bg-[#ffffff70] px-2 py-5 w-full gap-8 z-10 opacity-90' onKeyDown={(e)=>formKeyNotSubmit(e)}>
             {/* title */}
             <div className='flex flex-col gap-2 w-full'>
-                <label htmlFor="title" className='font-semibold text-[#2e424a]'>عنوان</label>
+                <label htmlFor="title" className=' font-[vasirMedium] text-[#2e424a]'>عنوان</label>
                 <input type="text" className='p-1  outline-[#0ab694] w-full' ref={titleRef} required={true} name='title'/>
             </div>
            {/* describe */}
             <Editor setDesc={setDesc}/>
             {/* category */}
              <div className='flex flex-col gap-2 w-full'>
-                <label htmlFor="supervisor" className='font-semibold text-[#2e424a]'>دسته بندی نمونه کار</label>
+                <label htmlFor="supervisor" className='font-[vasirMedium] font-[vasirMedium] text-[#2e424a]'>دسته بندی نمونه کار</label>
                 <select id="workSampleCategory" ref={workSampleCategoryRef} className='py-1'>
                     <option value="null">{"..."}</option>
                     {
-                        // workSampleCategories.map(work => ( console.log(work)
-                        //     // <option value={work.id}>{work.title}</option>
-                        // ))
+                        workSampleCategories.map(work => (
+                            <option value={work.id}>{work.title}</option>
+                        ))
                     }
                 </select>
-                <button onClick={()=>{setAddCategory(true)}} className='w-[100%] bg-[#07C7A3] transition-all duration-300 hover:shadow-[0px_0px_5px_1px_rgba(0,0,0,0.2)] hover:bg-[#0eecc3] text-white py-1 rounded-sm w-[20%] '>
+                <button onClick={()=>{setAddCategory(true)}} className='w-[100%] font-[vasirMedium] bg-[#07C7A3] transition-all duration-300 hover:shadow-[0px_0px_5px_1px_rgba(0,0,0,0.2)] hover:bg-[#0eecc3] text-white py-1 rounded-sm w-[20%] '>
                     اضافه کردن دسته بندی
              </button>
-             <button onClick={()=>{setDeleteCategory(true)}} className='w-[100%] bg-[#f92b35] transition-all duration-300 hover:shadow-[0px_0px_5px_1px_rgba(0,0,0,0.2)] hover:bg-[#f92b00] text-white  py-1 rounded-sm w-[20%] '>
+             <button onClick={()=>{setDeleteCategory(true)}} className='w-[100%] font-[vasirMedium] bg-[#f92b35] transition-all duration-300 hover:shadow-[0px_0px_5px_1px_rgba(0,0,0,0.2)] hover:bg-[#f92b00] text-white  py-1 rounded-sm w-[20%] '>
                 حذف دسته بندی
              </button>
             </div> 
            <div className='flex w-full flex-col items-center mt-5 gap-3'>
-             <div className='flex items-center w-[80%] sm:w-[50%] 2xl:w-[30%] justify-between'>
+             <div className='flex items-center w-[80%] sm:w-[50%] 2xl:w-[30%] font-[vasirMedium] justify-between'>
              <button type='button' onClick={(e) => formSubmitter(e)} className='w-[100%] bg-[#07C7A3] transition-all duration-300 hover:shadow-[0px_0px_5px_1px_rgba(0,0,0,0.2)] hover:bg-[#0eecc3] text-white font-bold text-xl py-1 rounded-sm'>
                 {
                     loading
